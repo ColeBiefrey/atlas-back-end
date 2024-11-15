753dbc2b25f9d8883c2342b63ee9f033d4d38d5c
Cole Kelsey <Cole.Kelsey@atlasschool.com>
2024-11-15 02:23:16 -0600

    condensed into one single syntax based on advice

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,46 +3,44 @@
 import requests
 import sys
 
-
-def fetch_user_data(user_id):
+def get_employee_todo_progress(employee_id):
     """ fetchs API information """
-    user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-    response = requests.get(user_url)
-    return response.json()
-
-
-def fetch_user_tasks(user_id):
-    """ fetchs user tasks """
-    todo_url = f'https://jsonplaceholder.typicode.com/todos/?userId={user_id}'
-    response = requests.get(todo_url)
-    return response.json()
-
-
-def get_completed_tasks(tasks):
-    """ extract tasks from main list """
-    return [task['title'] for task in tasks if task['completed']]
-
-
-def print_completed_tasks(user_name, completed_tasks, total_tasks):
-    """ prints tasks """
-    print(f'Employee {user_name} is done with tasks({len(completed_tasks)}/{total_tasks}):')
+    employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    todo_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    
+    employee_response = requests.get(employee_url)
+    if employee_response.status_code != 200:
+        print(f"Employee with ID {employee_id} not found.")
+        return
+    
+    employee_data = employee_response.json()
+    employee_name = employee_data['name']
+    
+    todo_response = requests.get(todo_url)
+    if todo_response.status_code != 200:
+        print(f"Couldn't fetch todo list for employee ID {employee_id}.")
+        return
+    
+    todo_data = todo_response.json()
+    
+    total_tasks = len(todo_data)
+    completed_tasks = [task for task in todo_data if task['completed']]
+    num_done_tasks = len(completed_tasks)
+    
+    print(f"Employee {employee_name} is done with tasks({num_done_tasks}/{total_tasks}):")
+    
     for task in completed_tasks:
-        print(f'\t {task}')
-
-
-def employee_todo(user_id):
-    """ function that gathers and utilizes information """
-    user_data = fetch_user_data(user_id)
-    user_name = user_data.get('name')
-    tasks = fetch_user_tasks(user_id)
-    completed_tasks = get_completed_tasks(tasks)
-    print_completed_tasks(user_name, completed_tasks, len(tasks))
-
+        print(f"\t {task['title']}")
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        try:
-            user_id = int(sys.argv[1])
-            employee_todo(user_id)
-        except ValueError:
-            print("Please provide a valid number user ID.")
+    if len(sys.argv) != 2:
+        print("EMPLOYEE_ID")
+        sys.exit(1)
+    
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an valid number")
+        sys.exit(1)
+    
+    get_employee_todo_progress(employee_id)
