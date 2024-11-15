131c5fe6042420f51bb9836d9e85dbf86afc0430
Cole Kelsey <Cole.Kelsey@atlasschool.com>
2024-11-15 02:28:40 -0600

    typo

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -28,8 +28,7 @@ def get_employee_todo_progress(employee_id):
     completed_tasks = [task for task in todo_data if task['completed']]
     num_done_tasks = len(completed_tasks)
 
-    print(f"Employee {employee_name} is done with tasks
-            ({num_done_tasks}/{total_tasks}):")
+    print(f"{employee_name} finished({num_done_tasks}/{total_tasks}):")
 
     for task in completed_tasks:
         print(f"\t {task['title']}")
