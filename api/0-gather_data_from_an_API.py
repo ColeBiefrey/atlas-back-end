884323b9fd7ad8bd09b6d82f7fb19353b6a3b68e
Cole Kelsey <Cole.Kelsey@atlasschool.com>
2024-11-15 02:40:53 -0600

    checker indentation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,25 +5,25 @@ import sys
 
 
 def fetch_data(url):
-""" geter for API information """
+    """ geter for API information """
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
 
 def get_user_name(user_id):
-""" API handler for usernames """
+    """ API handler for usernames """
     user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
     user_data = fetch_data(user_url)
     return user_data.get('name')
 
 def get_completed_tasks(user_id):
-""" API Handler for tasks """
+    """ API Handler for tasks """
     todo_url = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
     todos = fetch_data(todo_url)
     return [task.get('title') for task in todos if task.get('completed')]
 
 def employee_todo():
-""" lists todo of the employees and compeltions """
+    """ lists todo of the employees and compeltions """
     try:
         user_id = sys.argv[1]
         name = get_user_name(user_id)
@@ -38,6 +38,7 @@ def employee_todo():
         print(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
+    """ usage handler """
     if len(sys.argv) == 2:
         employee_todo()
     else:
