28d802c2559704aff66b582aa5d7180dc1790b30
Cole Kelsey <Cole.Kelsey@atlasschool.com>
2024-11-15 02:41:56 -0600

    pep

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,11 +10,13 @@ def fetch_data(url):
     response.raise_for_status()
     return response.json()
 
+
 def get_user_name(user_id):
     """ API handler for usernames """
     user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
     user_data = fetch_data(user_url)
     return user_data.get('name')
+
 
 def get_completed_tasks(user_id):
     """ API Handler for tasks """
@@ -22,13 +24,14 @@ def get_completed_tasks(user_id):
     todos = fetch_data(todo_url)
     return [task.get('title') for task in todos if task.get('completed')]
 
+
 def employee_todo():
     """ lists todo of the employees and compeltions """
     try:
         user_id = sys.argv[1]
         name = get_user_name(user_id)
         tasks = get_completed_tasks(user_id)
-        print(f'Employee {name} is done with tasks({len(tasks)}/{len(tasks)}):')
+        print(f'Employee {name} is tasks({len(tasks)}/{len(tasks)}):')
         print('\n'.join(f'\t {task}' for task in tasks))
     except requests.RequestException as e:
         print(f"An error occurred: {e}")
@@ -36,6 +39,7 @@ def employee_todo():
         print("Please provide a user ID as a command-line argument.")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
+
 
 if __name__ == "__main__":
     """ usage handler """
